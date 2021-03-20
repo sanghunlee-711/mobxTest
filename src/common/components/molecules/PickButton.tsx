9df1aefbd65f8a7836fad2e27fc4c51bc57a562c
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-03-20 23:59:57 +0900

    Add: Validation - 아이디, 비밀번호 비밀번호 확인 추가 - 성별 클릭기능 - 생년월일 및 좋아하는 브랜드는 추후 추가 필요

---
+++ src/common/components/molecules/PickButton.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface PickButton {
+    text?: string;
+    width?: string;
+    height?: string;
+    margin?: string;
+    fontSize?: string;
+}
+
+const PickButton: React.FC<PickButton> = ({ text, width, height, margin, fontSize }): JSX.Element => {
+    return (
+        <ButtonWrapper width={width} height={height} margin={margin} fontSize={fontSize}>
+            {text ? text : 'HelloHter'}
+        </ButtonWrapper>
+    );
+};
+
+const ButtonWrapper = styled.button<{
+    width?: string;
+    height?: string;
+    text?: string;
+    margin?: string;
+    fontSize?: string;
+}>`
+    border: 1px solid black;
+    width: ${(props) => (props.width ? props.width : '11vw')};
+    height: ${(props) => (props.height ? props.height : '5vh')};
+    font-size: ${(props) => (props.fontSize ? props.fontSize : '16px')};
+
+    text-align: center;
+    font-family: 'Playfair Display', serif;
+    margin: ${(props) => (props.margin ? props.margin : '0')};
+    background-color: white;
+    color: black;
+    transition: all 0.5s ease-in-out;
+
+    &:hover {
+        color: white;
+        background-color: black;
+    }
+`;
+
+export default PickButton;
--- src/pages/Register/template/RegisterTemplate.tsx
+++ src/pages/Register/template/RegisterTemplate.tsx
@@ -1,23 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Button from '../../../common/components/molecules/Button';
+import { idRegEx, pwRegEx } from '../../../common/constants/constant';
 
 const RegisterTemplate: React.FC = (): JSX.Element => {
     const [idValue, setIdValue] = useState('');
     const [pwValue, setpwValue] = useState('');
     const [pwReValue, setpwReValue] = useState('');
+    const [gender, setGender] = useState('');
     const [birthValue, setbirthValue] = useState('');
     const [brandValue, setBrandValue] = useState('');
 
+    const [idTestBool, setIdTestBool] = useState(false);
+    const [pwTestBool, setPwTestBool] = useState(false);
+    const [samePw, setSamePw] = useState(false);
+
+    const checkId = () => {
+        const idValidation: boolean = idRegEx.test(idValue.toLowerCase());
+        idValidation ? setIdTestBool(true) : setIdTestBool(false);
+    };
+    useEffect(() => {
+        pwValue === pwReValue ? setSamePw(true) : setSamePw(false);
+    }, [pwValue, pwReValue]);
+
+    const checkPw = () => {
+        const pwValidation: boolean = pwRegEx.test(pwValue);
+        pwValidation ? setPwTestBool(true) : setPwTestBool(false);
+        console.log(`pw: ${pwValue} re: ${pwReValue}`);
+    };
+
+    const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        const { name } = e.target as HTMLButtonElement;
+
+        switch (name) {
+            case 'men':
+                setGender(name);
+                break;
+            case 'women':
+                setGender(name);
+                break;
+
+            default:
+                throw new Error('Not Input');
+        }
+
+        console.log(name);
+    };
+
     const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target as HTMLInputElement;
-        console.log(idValue);
         switch (name) {
             case 'id':
                 setIdValue(value);
+                checkId();
                 break;
             case 'pw':
                 setpwValue(value);
+                checkPw();
                 break;
             case 'pwRe':
                 setpwReValue(value);
@@ -42,21 +81,33 @@ const RegisterTemplate: React.FC = (): JSX.Element => {
                 </InputWrapper>
                 <Button text="중복체크" margin="0 10px" />
             </InputContainer>
-            <ShowText>Ex)style@stylefolks.com</ShowText>
+            <ShowText idTestBool={idTestBool}>Ex)style@stylefolks.com</ShowText>
 
             <InputContainer>
                 <label htmlFor="pwInput">비밀번호</label>
-                <input name="pw" id="pwInput" type="text" value={pwValue} onChange={(e) => onChangeHandler(e)} />
+                <input name="pw" id="pwInput" type="password" value={pwValue} onChange={(e) => onChangeHandler(e)} />
             </InputContainer>
+            <ShowText pwTestBool={pwTestBool}>숫자와 특수문자를 포함한 8자 이상 20자 이내로 해주세요</ShowText>
+
             <InputContainer>
                 <label htmlFor="pwReInput">비밀번호 확인</label>
-                <input name="pwRe" id="pwReInput" type="text" value={pwReValue} onChange={(e) => onChangeHandler(e)} />
+                <input
+                    name="pwRe"
+                    id="pwReInput"
+                    type="password"
+                    value={pwReValue}
+                    onChange={(e) => onChangeHandler(e)}
+                />
             </InputContainer>
-            <ShowText>비밀번호가 동일하지 않습니다.</ShowText>
+            <ShowText samePw={samePw}>비밀번호가 동일하지 않습니다.</ShowText>
             <InputContainer>
                 <p>성별</p>
-                <Button text="Men" margin="10px 10px 10px 0" />
-                <Button text="Women" margin="10px" />
+                <PickButton gender={gender} onClick={(e) => onClickHandler(e)} name="men" margin="10px 10px 10px 0">
+                    Men
+                </PickButton>
+                <WomenButton gender={gender} onClick={(e) => onClickHandler(e)} name="women" margin="10px">
+                    Women
+                </WomenButton>
             </InputContainer>
             <InputContainer>
                 <label htmlFor="birthInput">생년월일</label>
@@ -98,16 +149,45 @@ const RegisterContainer = styled.section`
     font-family: 'Playfair Display', serif;
 `;
 
-const ShowText = styled.div`
+const ShowText = styled.div<{ idTestBool?: boolean; pwTestBool?: boolean; samePw?: boolean }>`
     font-size: 1rem;
     color: red;
     width: 100%;
-    visibility: visible;
+    transition: all 0.5s ease-in-out;
+    visibility: ${({ idTestBool, pwTestBool, samePw }) => (idTestBool || pwTestBool || samePw ? 'hidden' : 'visible')};
+    opacity: ${({ idTestBool, pwTestBool, samePw }) => (idTestBool || pwTestBool || samePw ? '0' : '1')};
 `;
 
 const InputWrapper = styled.div`
     display: flex;
     flex-direction: column;
+`;
+
+const PickButton = styled.button<{ margin?: string; gender?: string }>`
+    border: 1px solid black;
+    border: 1px solid black;
+    width: 11vw;
+    height: 5vh;
+    font-size: 16px;
+
+    text-align: center;
+    font-family: 'Playfair Display', serif;
+    margin: ${(props) => (props.margin ? props.margin : '0')};
+    background-color: white;
+    color: black;
+    transition: all 0.5s ease-in-out;
+    color: ${({ gender }) => (gender === 'men' ? 'white' : 'black')};
+    background-color: ${({ gender }) => (gender === 'men' ? 'black' : 'white')};
+
+    &:hover {
+        color: white;
+        background-color: black;
+    }
+`;
+
+const WomenButton = styled(PickButton)`
+    background-color: ${({ gender }) => (gender === 'women' ? 'black' : 'white')};
+    color: ${({ gender }) => (gender === 'women' ? 'white' : 'black')};
 `;
 
 const InputContainer = styled.div`
